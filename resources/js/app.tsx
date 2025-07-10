cfb52b33fe16ff03f1f3413d0820c086f0b89dd7
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2025-07-10 22:40:59 +0200

    add scss

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,4 +1,5 @@
 import '../css/app.css';
+import '../css/app.scss';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
