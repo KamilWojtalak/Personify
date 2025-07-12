e45cbc481b5b16f3c112203625627b10307370c8
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2025-07-12 17:48:38 +0200

    update h1

--- resources/js/layouts/default-layout.tsx
+++ resources/js/layouts/default-layout.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@inertiajs/react';
 import { type ReactNode } from 'react';
 
 interface DefaultLayoutProps {
@@ -23,7 +24,9 @@ export default ({ children, ...props }: DefaultLayoutProps) => (
                         d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                     />
                 </svg>
-                <h1 className="text-xl font-bold text-slate-900">Personify</h1>
+                <Link href={route('home')} className="text-xl font-bold text-slate-900">
+                    <h1>Personify</h1>
+                </Link>
             </div>
             <nav className="flex-1 space-y-2">
                 <a href="#" className="flex items-center gap-3 rounded-lg bg-indigo-50 px-3 py-2 font-semibold text-indigo-700">
