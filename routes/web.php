f1cf19bffde2ee79ea982009ca36552c7b8b20af
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2025-07-12 15:43:49 +0200

    add contacts route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\StaticPageController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', [StaticPageController::class, 'welcome'])->name('home');
+Route::resource('/contacts', ContactController::class);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
