a02c42c66c0e6eae166bbfe381e99e73fc4df273
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2025-07-12 15:46:09 +0200

    add welcome method

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\WelcomeContactResource;
+use App\Http\Resources\WelcomePersonaTypeResource;
+use App\Services\Models\ContactService;
+use App\Services\Models\PersonaTypeService;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class StaticPageController extends Controller
 {
+    public function __construct(
+        private ContactService $contactService,
+        private PersonaTypeService $personaTypeService
+    ) {
+        //
+    }
+
     public function welcome(): Response
     {
-        return Inertia::render('welcome');
+        $latestContacts = $this->contactService->latest();
+        $personaTypes = $this->personaTypeService->tree();
+
+        return Inertia::render('Welcome', [
+            'latestContacts' => WelcomeContactResource::collection($latestContacts),
+            'personaTypes' => WelcomePersonaTypeResource::collection($personaTypes),
+        ]);
     }
 }
